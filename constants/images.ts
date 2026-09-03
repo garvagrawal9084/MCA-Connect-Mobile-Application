bd1bd2e17742cf711a77039743946fab47312fb5
garvagrawal9084 <garvagrawal9084@gmail.com>
2026-09-03 12:42:45 +0530

    CLeaning repo

--- constants/images.ts
+++ constants/images.ts
@@ -3,15 +3,8 @@
  */
 import logo from "@/assets/images/logo.png";
 
-import appIcon from "@/assets/images/icon.png";
-import splashIcon from "@/assets/images/splash-icon.png";
-import androidIconForeground from "@/assets/images/android-icon-foreground.png";
-
 export const images = {
   logo,
-  appIcon,
-  splashIcon,
-  androidIconForeground,
 };
 
 export default images;
